9ad462ac185b7d693180d809e5b16c40a1af5ad9
Rene Bartkowiak <info@rene-bartkowiak.com>
2023-09-06 13:38:29 +0200

    Initial commit

---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+
+const email = ref("");
+const password = ref("");
+
+const login = async () => {
+  const { error } = await supabase.auth.signInWithPassword({
+    email: email.value,
+    password: password.value,
+  });
+
+  if (!error) {
+    await navigateTo("/");
+  }
+};
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="email" />
+    <input type="password" v-model="password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
